9e0c424aa3993e77e5e8015dc55388f3ec402a10
KennyVargas <kennykysv@gmail.com>
2023-07-22 12:22:42 -0300

    Added function to convert text into arrays and function to show it

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 class VerificaFuncao {
 
@@ -33,6 +34,33 @@ VerificaFuncao v0.0.1
     return conteudo.toString();
   }
 
+  public static int[] processarConteudo( String conteudo) {
+    if (conteudo == null || conteudo.isEmpty()) {
+      return null;
+    }
+    String[] numerosStr = conteudo.split(",");
+    int[] numeros = new int[numerosStr.length];
+
+    for (int  i=0; i < numerosStr.length; i++) {
+      numeros[i] = Integer.parseInt(numerosStr[i].trim());
+    }
+    return numeros;
+  }
+
+  public static void lerConteudo(String conteudo) {
+    if (conteudo != null) {
+      int[] numeros = processarConteudo(conteudo);
+      if (numeros != null) {
+        System.out.println("Números lidos do arquivo:");
+        System.out.println(Arrays.toString(numeros));
+      } else {
+        System.out.println("Não foram encontrados números.");
+      }
+    } else {
+        System.out.println("Não foi possível ler o arquivo.");
+    }
+  }
+
   public static void main(String args[]) {
     if ( args.length == 0 || args[0].equals("--help") || args[0].equals("-H")) {
       ShowHelp();
@@ -40,12 +68,18 @@ VerificaFuncao v0.0.1
       if ( args[0].equals("--version") || args[0].equals("-V")) {
         ShowVersion();
       } else {
-        System.out.println("argumento 1:" + args[0]);
         String dominio = lerArquivoTexto(args[0]);
-        System.out.println("argumento 2:" + args[1]);
         String imagem = lerArquivoTexto(args[1]);
+ 
+        System.out.println("argumento 1:" + args[0]);
+        System.out.println("argumento 2:" + args[1]);
+
         System.out.println("Dominio da função: " + dominio);
         System.out.println("Imagem da função: " + imagem);
+
+        lerConteudo(dominio);
+        lerConteudo(imagem);
+       
       }
     }
   }
